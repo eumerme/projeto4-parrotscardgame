21db43e858d6bcba66f000581922a74571441c95
eumerme <euthaisetau@gmail.com>
2022-06-16 20:13:22 -0300

    build: distribuição aleatória dos gifs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let popUp = Number(prompt("Com quantas cartas vocês quer jogar?")); //QUANTAS CARTAS
 let restriction = (isNaN(popUp) || popUp < 4 || popUp > 14 || (popUp % 2 !== 0)); //RESTRIÇÕES PARA NÃO ENTRAR NO JOGO
+let gif = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot","revertitparrot", "tripletsparrot", "unicornparrot"];
+    gif = gif.sort(comparador);
 
 // choose number of cards
 function numberCards (){
@@ -16,24 +18,40 @@ numberCards();
 
 //QUANTIDADE DE CARTAS SELECIONADAS na tela
 function boardGame () {
-    document.querySelector(".game-container").innerHTML = ""
-
-    for (let i = 0; i < popUp; i++){
-        const allCards = `
-            <div class="card-container" onclick="select (this);">
-                <div class="front-face card">
+    gif = gif.slice(0, (popUp/2));
+    gif = gif.concat(gif);
+    
+    let deck = document.querySelector(".game-container")
+    gif = gif.sort(comparador);
+    for (let i = 0; i < popUp; i++){        
+        deck.innerHTML += `
+            <div class="card" onclick="select (this);" data-identifier="card">
+                <div class="front-face face" data-identifier="back-face" >
                     <img src="./images/front.png" alt="">
                 </div>
-                <div class="back-face card">
-                    <img src="./images/bobrossparrot.gif" alt="">
+                <div class="back-face face" data-identifier="front-face">
+                    <img src="./images/${gif[i]}.gif" alt="">
                 </div>   
              </div>
-            `;
-        document.querySelector(".game-container").innerHTML += allCards;
+            `;     
     }
 }
 
 //VIRAR CARTA ao clicar
 function select (elemento) {
     elemento.classList.toggle("flip");   
+}
+
+//end of the game
+function congrats (){
+    const end = document.querySelectorAll(".card.flip");
+
+    if (Number(end.length ) === popUp){
+        alert("Você ganhou em X jogadas!"); //DESCOBRIR A QUANTIDADE DE CARTAS VIRADAS
+    }
+}
+
+//notion
+function comparador() { 
+	return Math.random() - 0.5; 
 }
